24277b7c8af2f73abf63900d02733b3b1ca08193
Ryan Bell <2752881B@student.gla.ac.uk>
2024-03-08 20:28:04 +0000

    Clear Search

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,5 +5,17 @@ $(document).ready(function() {
     $('#search-button').click(function(e) {
         var queryTerm = $('#search-bar').val();
         $('#search-query-parameter').attr("value", queryTerm);
+        if($('#page-form').length) {
+            $('#page-form').trigger("submit");
+            e.preventDefault();
+        }
+    });
+    $('#reset-button').click(function(e) {
+        $('#search-bar').val("");
+        if($('#page-form').length) {
+            $('#page-form').trigger("submit");
+        } else {
+            $('#search-form').trigger("submit");
+        }
     });
 });

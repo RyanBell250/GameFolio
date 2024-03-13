982771ba8a7c0399eb62b9f6f7ee348afdc64f23
Dylan <dhdylanhuang@gmail.com>
2024-03-13 17:35:30 +0000

    Completely Updated Views for Correct Registration

--- gamefolio_app/urls.py
+++ gamefolio_app/urls.py
@@ -5,6 +5,10 @@ app_name = 'gamefolio_app'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    
+    path('accounts/register/', views.MyRegistrationView.as_view(), name='registration_register'),
+    path('register_profile/', views.RegisterProfileView.as_view(), name='register_profile'),  # Corrected line
+
     path('profile/<username>/', views.ProfileView.as_view(), name='profile'),
     path('profiles/', views.ListProfilesView.as_view(), name='list_profiles'),
     path('search/', views.SearchView.as_view(), name='search'),

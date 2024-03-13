7e1691cb15ec6d18839f8d64c499ced85999ca53
Dylan <dhdylanhuang@gmail.com>
2024-03-13 11:40:29 +0000

    Removed unnecessary code for django-registration-redux

--- gamefolio_app/urls.py
+++ gamefolio_app/urls.py
@@ -5,10 +5,6 @@ app_name = 'gamefolio_app'
 
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    
-    path('register_profile/', views.RegisterProfileView.as_view(), name='register_profile'),
-    path('accounts/register/', views.MyRegistrationView.as_view(), name='register_registration'),
-    
     path('profile/<username>/', views.ProfileView.as_view(), name='profile'),
     path('profiles/', views.ListProfilesView.as_view(), name='list_profiles'),
 

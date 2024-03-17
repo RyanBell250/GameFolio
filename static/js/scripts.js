9d0649e31a800afb6097ce9d718aed3f7dcdbe1a
Ryan Bell <2752881B@student.gla.ac.uk>
2024-03-17 14:14:07 +0000

    Possible rating stars, not functional, only visual

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 
     const searchParams = new URLSearchParams(window.location.search);
-
+    var ratingConfirmed = false;
+    
     $('#search-button').click(function(e) {
         var queryTerm = $('#search-bar').val();
         $('#search-query-parameter').attr("value", queryTerm);
@@ -16,8 +17,43 @@ $(document).ready(function() {
             $('#page-form').trigger("submit");
         }
     });
+
+    //Ratings
+    function calculateSize(e) {
+        var mouseX = e.pageX;
+        var left = $("#rating-hover").position().left;
+        var width = $("#rating").width();
+        var relativeMouseX = mouseX-left;
+        var percentage = relativeMouseX/width + 0.095;
+        if(percentage < 0.1) {
+            percentage = 0.1;
+        }
+
+        const halfStarSize = width/10;
+        var newWidth = width * (percentage);
+        var newWidth = newWidth - (newWidth % halfStarSize)
+
+        return newWidth;
+    }
+
+    $('#rating').mousemove(function(e) {
+        $("#rating-actual").addClass("d-none");
+        $("#rating-hover").get(0).style.setProperty("--width", calculateSize(e)+"px");
+    });
     
+    $('#rating').mouseleave(function(e) {
+        $("#rating-actual").removeClass("d-none");
+        $("#rating-hover").get(0).style.setProperty("--width", "0px");
+    });
+
+    $('#rating').click(function(e) {
+        $("#rating-actual").removeClass("d-none");
+        $("#rating-actual").get(0).style.setProperty("--width", calculateSize(e) + "px");
+        $("#rating-hover").get(0).style.setProperty("--width", "0px");
+    });
+
     //Tooltips
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 });
+

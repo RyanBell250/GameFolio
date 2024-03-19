c552f094bdac64ff9adda245d091b371fd618e21
Dylan <dhdylanhuang@gmail.com>
2024-03-19 17:52:54 +0000

    Updated lists, add and remove games and delete list

--- gamefolio_app/urls.py
+++ gamefolio_app/urls.py
@@ -14,7 +14,9 @@ urlpatterns = [
     
     path('lists/', views.ListsView.as_view(), name='lists'),
     path('create_list/', views.CreateListView.as_view(), name='create_list'),
+    path('<str:author_username>/<str:list_title>/<slug:slug>/delete/', views.ListDeleteView.as_view(), name='list_delete'),
     path('list/<author_username>/<list_title>/<slug:slug>', views.ListView.as_view(), name='list'),
+    path('list/<str:author_username>/<str:list_title>/<str:slug>/remove_game/', views.RemoveGameView.as_view(), name='remove_game'),
     
     path('search/', views.SearchView.as_view(), name='search'),
     path('404/', views.NotFoundView.as_view(), name='404'),

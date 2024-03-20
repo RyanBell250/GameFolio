dcc7fef55cfbb38a741806bb23a65a6832abdea7
Ryan Bell <2752881B@student.gla.ac.uk>
2024-03-20 15:41:53 +0000

    Game card moved to inclusion tag for easy re-use

--- gamefolio_app/templatetags/tags.py
+++ gamefolio_app/templatetags/tags.py
@@ -5,3 +5,17 @@ register = template.Library()
 @register.simple_tag
 def get_image(game, image_type):
     return game.get_image(image_type)
+
+@register.inclusion_tag("gamefolio_app/game_card.html", takes_context=True)
+def render_game_card(context, game, *args, **kwargs):
+
+    context_dict = kwargs
+    context_dict["game"] = game;
+    context_dict["user"] = context["user"]
+    print(kwargs)
+
+    return context_dict
+
+@register.inclusion_tag("gamefolio_app/review.html")
+def render_review(review):
+    return {"review": review}

7e9af96b1e8936c1f467fba83936380501a4e6ae
Ryan Bell <2752881B@student.gla.ac.uk>
2024-03-21 04:28:57 +0000

    List Icons

--- gamefolio_app/templatetags/tags.py
+++ gamefolio_app/templatetags/tags.py
@@ -1,10 +1,18 @@
 from django import template
+from gamefolio_app.models import ListEntry
 
 register = template.Library()
 
 @register.simple_tag
 def get_image(game, image_type):
     return game.get_image(image_type)
+
+@register.inclusion_tag("gamefolio_app/list_image.html")
+def render_list_images(list):
+    entries = ListEntry.objects.filter(list = list)[:4];
+    if(len(entries) < 4):
+        entries =[entries[0]];
+    return {"entries": entries, "list": list}
 
 @register.inclusion_tag("gamefolio_app/game_card.html", takes_context=True)
 def render_game_card(context, game, *args, **kwargs):

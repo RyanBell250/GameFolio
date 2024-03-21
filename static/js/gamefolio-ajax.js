0741c2e6b77ad22eea70630872176a4bf3d4071f
Ryan Bell <2752881B@student.gla.ac.uk>
2024-03-21 21:40:06 +0000

    Fixed an issue with the wrong thing appearing in search bar

--- static/js/gamefolio-ajax.js
+++ static/js/gamefolio-ajax.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
             });
         },
         select: function(event, ui) {
-            $('#search-query-parameter').attr("value", ui.item.label);
+            $('#search-query-parameter').attr("value", ui.item.value);
             $("#search-form").submit();
         },
         minLength: 0
@@ -29,15 +29,16 @@ $(document).ready(function() {
             });
         },
         select: function(event, ui) {
-            var query;
-            query = $("#list-search-bar").val();
             $.ajax({
-                url: '/gamefolio/get-game/?id='+ui.item.value,
+                url: '/gamefolio/get-game/?id='+ui.item.id,
                 success: function(data) {
-                    $("#list-games").append(data);
                     $("#list-search-bar").val("");
+                    if(!$("#" + ui.item.id).length) {
+                        $("#list-games").append(data);
+                    } else {
+                        alert("Game already exists in list.")
+                    }
                     return false;
-                    return data;
                 }
             })
         },

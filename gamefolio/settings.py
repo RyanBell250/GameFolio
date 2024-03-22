7586851c7fa0022fb119c7f09df99f1e4c70de1e
2775752h <2775752h@student.gla.ac.uk>
2024-03-22 01:33:03 +0000

    Altered Allowed Hosts

--- gamefolio/settings.py
+++ gamefolio/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '68y1in%#_2lwq2w^qw3tm!rpw@=3wgnm82rwyp_amf_!akh3c9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['2775752h.eu.pythonanywhere.com']
 
 # Registration config
 
